2747f4c430d61c9cc6ee4eca77bfb8e3a6764ec3
GlebasusBP <bgp13art@Gmail.com>
2022-05-30 13:43:34 +0300

    refactor: add alt in img elem according to rule 1.2 advanced  in html-and-css.md

--- app.js
+++ app.js
@@ -48,6 +48,7 @@ var createNewTaskElement=function(taskString){
     deleteButton.className="btn delete";
     deleteButtonImg.src='./remove.svg';
     deleteButtonImg.className = "img";
+    deleteButtonImg.setAttribute('alt', 'remove');
     deleteButton.appendChild(deleteButtonImg);
 
 
